d94d7ae066c5ff6c6aaca88b0c8d950c5bb97af9
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-02-10 17:48:05 +0000

    Refactor operation 19

--- lib/virtual_machine.ex
+++ lib/virtual_machine.ex
@@ -1,6 +1,8 @@
 defmodule VirtualMachine do
 
   @bit_size 16
+  @register_reference_start 32768
+  @register_reference_end 32775
 
   def load_binary(raw_binary_input) do
     do_load_binary([], raw_binary_input)
@@ -24,9 +26,23 @@ defmodule VirtualMachine do
     current_instruction = Enum.at(instructions, cursor)
     case current_instruction do
       0 -> exit (:shutdown)
-      19 -> IO.puts([Enum.at(instructions, cursor + 1)])
-      21 -> do_run_instructions(cursor + 1, instructions, registers, stack)
+      19 ->
+        character_to_print = fUNCTION_TO_BE_NAMED(cursor+1, instructions, registers)
+        IO.puts([character_to_print])
+      21 -> do_run_instructions(cursor+1, instructions, registers, stack)
     end
+  end
+
+  def fUNCTION_TO_BE_NAMED(cursor, instructions, registers) do
+    if (register_address?(cursor)) do
+    else
+      Enum.at(instructions, cursor)
+    end
+  end
+
+  defp register_address?(address_or_value) do
+    address_or_value > @register_reference_start and
+    address_or_value < @register_reference_end
   end
 
 end
