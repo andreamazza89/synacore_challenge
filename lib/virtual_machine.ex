89bb6346739ebb57f1354cbf096c80d71eddc7ef
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-02-13 21:47:37 +0000

    Add operation 6

--- lib/virtual_machine.ex
+++ lib/virtual_machine.ex
@@ -1,48 +1,69 @@
 defmodule VirtualMachine do
 
   @bit_size 16
-  @register_reference_start 32768
-  @register_reference_end 32775
+  @address_start 32768
+  @address_end 32775
 
   def load_binary(raw_binary_input) do
     do_load_binary([], raw_binary_input)
   end
 
   defp do_load_binary(loaded_binary,
-                      <<current_value::little-integer-size(@bit_size), remaining_input::binary>>) do
-    do_load_binary(loaded_binary ++ [current_value], remaining_input)
+    <<current_value::little-integer-size(@bit_size), remaining_input::binary>>) do
+      do_load_binary(loaded_binary ++ [current_value], remaining_input)
   end
 
-  defp do_load_binary(loaded_binary, <<>>) do
+  defp do_load_binary(loaded_binary, "\n") do
     loaded_binary
   end
 
 
   def run_instructions(instructions) do
-    do_run_instructions(0, instructions, [[],[],[],[],[],[],[],[]], []) #### might want to make these Structs...
+    do_run_instructions(0, instructions, [[],[],[],[],[],[],[],[]], [])
   end
 
   def do_run_instructions(cursor, instructions, registers, stack) do
     current_instruction = Enum.at(instructions, cursor)
     case current_instruction do
-      0 -> exit (:shutdown)
+      0 ->
+        exit (:shutdown)
+      6 ->
+        new_cursor = jump_operation(cursor, instructions, registers)
+        do_run_instructions(new_cursor, instructions, registers, stack)
       19 ->
-        character_to_print = fUNCTION_TO_BE_NAMED(cursor+1, instructions, registers)
-        IO.puts([character_to_print])
-      21 -> do_run_instructions(cursor+1, instructions, registers, stack)
+        out_operation(cursor, instructions, registers)
+        do_run_instructions(cursor+2, instructions, registers, stack)
+      21 ->
+        do_run_instructions(cursor+1, instructions, registers, stack)
+      _ ->
+        IO.puts "**** operation not implemented: #{current_instruction} *****"
     end
   end
 
-  def fUNCTION_TO_BE_NAMED(cursor, instructions, registers) do
-    if (register_address?(cursor)) do
+  def jump_operation(cursor, instructions, registers) do
+    new_cursor_or_address = Enum.at(instructions, cursor+1)
+    new_cursor = get_argument_value(new_cursor_or_address, instructions, registers)
+  end
+
+  def out_operation(cursor, instructions, registers) do
+    char_value_or_address = Enum.at(instructions, cursor+1)
+    char_to_print = get_argument_value(char_value_or_address, instructions, registers)
+    IO.write([char_to_print])
+  end
+
+  def get_argument_value(address_or_value, instructions, registers) do
+    if (register_address?(address_or_value)) do
+      data_address = address_or_value - @address_start
+      [value] = Enum.at(registers, data_address)
+      value
     else
-      Enum.at(instructions, cursor)
+      address_or_value
     end
   end
 
   defp register_address?(address_or_value) do
-    address_or_value > @register_reference_start and
-    address_or_value < @register_reference_end
+    address_or_value >= @address_start and
+    address_or_value <= @address_end
   end
 
 end
--- test/virtual_machine_test.exs
+++ test/virtual_machine_test.exs
@@ -2,17 +2,43 @@ defmodule SynacoreChallengeTest do
   use ExUnit.Case
   import ExUnit.CaptureIO
 
+  #VM does not complain if it encounters values described as invalid in the arch-spec
+
+  test "run whole program" do
+    binary = File.read!("./docs/challenge.bin")
+    instructions = VirtualMachine.load_binary(binary)
+    VirtualMachine.run_instructions(instructions)
+  end
+
   test "loads little endian 16-bit binary input into memory" do
-    little_endian_binary_input = <<21, 0, 19, 15, 133, 1>>
+    little_endian_binary_input = <<21, 0, 19, 15, 133, 1, "\n">>
     loaded_input = [21, 3859, 389]
     assert VirtualMachine.load_binary(little_endian_binary_input) == loaded_input
   end
 
 
   test "loads another little endian 16-bit binary input into memory" do
-    little_endian_binary_input = <<19, 15, 133, 1, 21, 0>>
+    little_endian_binary_input = <<19, 15, 133, 1, 21, 0, "\n">>
     loaded_input = [3859, 389, 21]
     assert VirtualMachine.load_binary(little_endian_binary_input) == loaded_input
+  end
+
+  test "reads the value from first register if value is first register address" do
+    # register address starts at 32768
+    assert VirtualMachine.get_argument_value(32768, [6, 32768], [[55],[],[],[],[],[],[],[]])
+            == 55
+  end
+
+  test "reads the value from second register if value is first register address" do
+    # register address starts at 32768
+    assert VirtualMachine.get_argument_value(32769, [6, 32769], [[],[66],[],[],[],[],[],[]])
+            == 66
+  end
+
+  test "gives the value back if not to be read from registers" do
+    # register address starts at 32768
+    assert VirtualMachine.get_argument_value(444, [6, 444], [[],[],[],[],[],[],[],[]])
+            == 444
   end
 
   test "operation 0-halt: stops execution and terminates the program" do
@@ -20,17 +46,25 @@ defmodule SynacoreChallengeTest do
   end
 
   test "operation 19-out: prints the given (ASCII) value to the console" do
-    console_out = capture_io(fn() -> VirtualMachine.run_instructions([19, 88]) end)
-    assert console_out == "X\n"
+    console_out = capture_io(fn() ->
+      VirtualMachine.out_operation(0, [19, 88], [])
+    end)
+
+    assert console_out == "X"
   end
 
   test "operation 19-out: prints the referenced (ASCII) value to the console" do
-    #imlement me once you can write into registers
+    # register address starts at 32768
+    console_out = capture_io(fn() ->
+      VirtualMachine.out_operation(0, [19, 32768], [[89],[],[],[],[],[],[],[]])
+    end)
+
+    assert console_out == "Y"
   end
 
-  test "operation 21-noop: no operation (skips to the next index)" do
-    console_out = capture_io(fn() -> VirtualMachine.run_instructions([21, 19, 88]) end)
-    assert console_out == "X\n"
+  test "operation 6-jump: cursor is updated to the given memory address" do
+    new_cursor = VirtualMachine.jump_operation(0, [6, 66], [[],[],[],[],[],[],[],[]])
+    assert new_cursor == 66
   end
 
 end
